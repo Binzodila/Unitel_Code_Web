a7bc4304a3388ea0d554590ec1606a161d27eb0f
Binzodila <josebinzodila@gmail.com>
2022-03-24 22:38:37 +0000

    Primeiro commit do Projecto

---
+++ Script/envent.js
@@ -0,0 +1,25 @@
+
+  function aumentar(){
+    var a = document.getElementById('aumentar')
+    a.style.transform= 'scale(1.2)'
+}
+function sair(){
+    var a = document.getElementById('aumentar')
+    a.style.transform= 'scale(1)'
+}
+function aumentar1 (){
+    var a = document.getElementById('aumentar1')
+    a.style.transform= 'scale(1.2)'
+}
+function sair1(){
+    var a = document.getElementById('aumentar1')
+    a.style.transform= 'scale(1)'
+}
+function aumentar2(){
+    var a = document.getElementById('aumentar2')
+    a.style.transform= 'scale(1.2)'
+}
+function sair2(){
+    var a = document.getElementById('aumentar2')
+    a.style.transform= 'scale(1)'
+}
